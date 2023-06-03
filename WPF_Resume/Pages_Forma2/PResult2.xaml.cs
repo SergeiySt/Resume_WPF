28e923145912f069f8e10b729e90d8b0ad57d936
SergeiySt <ser.st.1989@gmail.com>
2023-06-03 12:40:33 +0300

    add_678

--- WPF_Resume/Pages_Forma2/PResult2.xaml.cs
+++ WPF_Resume/Pages_Forma2/PResult2.xaml.cs
@@ -23,86 +23,132 @@ namespace WPF_Resume.Pages_Forma2
     /// </summary>
     public partial class PResult2 : Page
     {
-        //PFIO2 pFIO2 = new PFIO2();
-        //PPhoto2 photo2 = new PPhoto2();
-        //PEdEx pEdEx = new PEdEx();
-        //PSkPq pSkPq = new PSkPq();
-        //PResult2 pResult2 = new PResult2();
+        PFIO2 pFIO2 = new PFIO2();
+        PPhoto2 photo2 = new PPhoto2();
+        PEdEx pEdEx = new PEdEx();
+        PSkPq pSkPq = new PSkPq();
         public PResult2()
         {
             InitializeComponent();
+
+
         }
 
-        private void buttonPrintDocx_Click(object sender, RoutedEventArgs e)
+        private byte[] GetImageBytesFromImageControl(System.Windows.Controls.Image imageControl)
         {
             try
             {
-                string filename = "output.docx";
-                using (DocX doc = DocX.Create(filename))
+                BitmapImage bitmapImage = (BitmapImage)imageControl.Source;
+                using (MemoryStream memoryStream = new MemoryStream())
                 {
-
-                    Formatting nameFormat = new Formatting { Size = 26, Bold = true };
-                    Xceed.Document.NET.Paragraph nameParagraph = doc.InsertParagraph(pFIO2.textBoxSurname.Text + " " + pFIO2.textBoxName.Text + " " + pFIO2.textBoxPobatkovi.Text, false, nameFormat);
-                    nameParagraph.Alignment = Alignment.left;
-
-
-                    Xceed.Document.NET.Paragraph infoParagraph = doc.InsertParagraph();
-                    infoParagraph.AppendLine("Дата рождения: " + pFIO2.datePicker.SelectedDate?.ToShortDateString());
-                    infoParagraph.AppendLine("Адрес: " + pFIO2.textBoxAdress.Text);
-                    infoParagraph.AppendLine("Телефон: " + pFIO2.textBoxPhone.Text);
-                    infoParagraph.AppendLine("Email: " + pFIO2.textBoxEmail.Text);
-
-
-                    Formatting educationFormat = new Formatting { Size = 18, Bold = true };
-                    Xceed.Document.NET.Paragraph educationHeaderParagraph = doc.InsertParagraph("Образование", false, educationFormat);
-                    Xceed.Document.NET.Paragraph educationParagraph = doc.InsertParagraph();
-                    educationParagraph.Append(new TextRange(pEdEx.richTextBoxEducation.Document.ContentStart, pEdEx.richTextBoxEducation.Document.ContentEnd).Text);
-                    educationParagraph.SpacingAfter(10);
-
-
-                    Formatting experienceFormat = new Formatting { Size = 18, Bold = true };
-                    Xceed.Document.NET.Paragraph experienceHeaderParagraph = doc.InsertParagraph("Опыт работы", false, experienceFormat);
-                    Xceed.Document.NET.Paragraph experienceParagraph = doc.InsertParagraph();
-                    experienceParagraph.Append(new TextRange(pEdEx.richTextBoxExperience.Document.ContentStart, pEdEx.richTextBoxExperience.Document.ContentEnd).Text);
-                    experienceParagraph.SpacingAfter(10);
-
-
-                    Formatting skillsFormat = new Formatting { Size = 18, Bold = true };
-                    Xceed.Document.NET.Paragraph skillsHeaderParagraph = doc.InsertParagraph("Навыки", false, skillsFormat);
-                    Xceed.Document.NET.Paragraph skillsParagraph = doc.InsertParagraph();
-                    skillsParagraph.Append(new TextRange(pSkPq.richTextBoxSkills.Document.ContentStart, pSkPq.richTextBoxSkills.Document.ContentEnd).Text);
-                    skillsParagraph.SpacingAfter(10);
-
-
-                    Formatting personalFormat = new Formatting { Size = 18, Bold = true };
-                    Xceed.Document.NET.Paragraph personalHeaderParagraph = doc.InsertParagraph("Личные качества", false, personalFormat);
-                    Xceed.Document.NET.Paragraph personalParagraph = doc.InsertParagraph();
-
-                    if (photo2.pictureBox.Source is BitmapImage bitmapImage)
-                    {
-                        byte[] imageBytes = GetImageBytesFromBitmapImage(bitmapImage);
-                        string imagePath = "image.png";
-
-                        File.WriteAllBytes(imagePath, imageBytes);
-
-                        Xceed.Document.NET.Image image = doc.AddImage(imagePath);
-                        Picture picture = image.CreatePicture();
-                        picture.Width = 200;
-                        picture.Height = 200;
-
-                        Xceed.Document.NET.Paragraph imageParagraph = doc.InsertParagraph();
-                        imageParagraph.InsertPicture(picture);
-                        imageParagraph.Alignment = Alignment.right;
-                    }
-
-                    doc.Save();
+                    BitmapEncoder encoder = new PngBitmapEncoder();
+                    encoder.Frames.Add(BitmapFrame.Create(bitmapImage));
+                    encoder.Save(memoryStream);
+                    return memoryStream.ToArray();
                 }
-
-                System.Windows.MessageBox.Show("Документ успешно создан.", "Успех");
             }
             catch (Exception ex)
             {
-                System.Windows.MessageBox.Show("Ошибка при создании документа: " + ex.Message, "Ошибка");
+                Console.WriteLine($"Error converting image to bytes: {ex.Message}");
+                return null;
+            }
+        }
+        private void buttonPrintDocx_Click(object sender, RoutedEventArgs e)
+        {
+            string surname = pFIO2.Surname;
+            string name = pFIO2.Name;
+            string pobatkovi = pFIO2.Pobatkovi;
+            string adress = pFIO2.Adress;
+            string date = pFIO2.Date.ToShortDateString();
+            string phone = pFIO2.PhoneNumber;
+            string email = pFIO2.Email;
+
+            byte[] imageBytes2 = GetImageBytesFromImageControl(photo2.pictureBox);
+            string education = pEdEx.Education;
+            string experience = pEdEx.Experience;
+            string skills = pSkPq.Skills;
+            string prsonal = pSkPq.Personal;
+
+
+            try
+            {
+                Microsoft.Win32.SaveFileDialog saveFileDialog = new Microsoft.Win32.SaveFileDialog();
+                saveFileDialog.Filter = "Word Document (*.docx)|*.docx";
+                saveFileDialog.FileName = "resume.docx";
+
+                bool? result = saveFileDialog.ShowDialog();
+                if (result == true)
+                {
+                    string filename = saveFileDialog.FileName;
+                    using (DocX doc = DocX.Create(filename))
+                    {
+                        // Ваш остальной код...
+
+                        Formatting nameFormat = new Formatting { Size = 26, Bold = true };
+                        Xceed.Document.NET.Paragraph nameParagraph = doc.InsertParagraph(surname + " " + name + " " + pobatkovi, false, nameFormat);
+                        nameParagraph.Alignment = Alignment.left;
+
+
+                        Xceed.Document.NET.Paragraph infoParagraph = doc.InsertParagraph();
+                        infoParagraph.AppendLine("Дата народження: " + date);
+                        infoParagraph.AppendLine("Адреса: " + adress);
+                        infoParagraph.AppendLine("Телефон: " + phone);
+                        infoParagraph.AppendLine("Email: " + email);
+
+
+                        Formatting educationFormat = new Formatting { Size = 18, Bold = true };
+                        Xceed.Document.NET.Paragraph educationHeaderParagraph = doc.InsertParagraph("Освіта", false, educationFormat);
+                        Xceed.Document.NET.Paragraph educationParagraph = doc.InsertParagraph();
+                        educationParagraph.Append(education);
+                        educationParagraph.SpacingAfter(10);
+
+
+                        Formatting experienceFormat = new Formatting { Size = 18, Bold = true };
+                        Xceed.Document.NET.Paragraph experienceHeaderParagraph = doc.InsertParagraph("Досвід роботи", false, experienceFormat);
+                        Xceed.Document.NET.Paragraph experienceParagraph = doc.InsertParagraph();
+                        experienceParagraph.Append(experience);
+                        experienceParagraph.SpacingAfter(10);
+
+
+                        Formatting skillsFormat = new Formatting { Size = 18, Bold = true };
+                        Xceed.Document.NET.Paragraph skillsHeaderParagraph = doc.InsertParagraph("Навички", false, skillsFormat);
+                        Xceed.Document.NET.Paragraph skillsParagraph = doc.InsertParagraph();
+                        skillsParagraph.Append(skills);
+                        skillsParagraph.SpacingAfter(10);
+
+                        Formatting personalFormat = new Formatting { Size = 18, Bold = true };
+                        Xceed.Document.NET.Paragraph spersonalHeaderParagraph = doc.InsertParagraph("Навички", false, skillsFormat);
+                        Xceed.Document.NET.Paragraph personalParagraph = doc.InsertParagraph();
+                        personalParagraph.Append(prsonal);
+                        personalParagraph.SpacingAfter(10);
+
+
+                        if (photo2.pictureBox.Source is BitmapImage bitmapImage)
+                        {
+                            byte[] imageBytes = GetImageBytesFromBitmapImage(bitmapImage);
+                            string imagePath = "image.png";
+
+                            File.WriteAllBytes(imagePath, imageBytes);
+
+                            Xceed.Document.NET.Image image = doc.AddImage(imagePath);
+                            Picture picture = image.CreatePicture();
+                            picture.Width = 200;
+                            picture.Height = 200;
+
+                            Xceed.Document.NET.Paragraph imageParagraph = doc.InsertParagraph();
+                            imageParagraph.InsertPicture(picture);
+                            imageParagraph.Alignment = Alignment.right;
+                        }
+
+                        doc.Save();
+                    }
+
+                    System.Windows.MessageBox.Show("Документ успішно створено.", "Успіх");
+                }
+            }
+            catch (Exception ex)
+            {
+                System.Windows.MessageBox.Show("Помилка при створення документа: " + ex.Message, "Помилка");
             }
         }
 
